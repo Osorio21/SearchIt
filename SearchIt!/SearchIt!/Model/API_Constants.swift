e61b8218fb86d9482219727627e98f2602b139cc
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-07 19:06:27 -0500

    Struct containing string constants used in FourSquare Places API call

---
+++ SearchIt!/SearchIt!/Model/API_Constants.swift
@@ -0,0 +1,22 @@
+//
+//  API_Constants.swift
+//  SearchIt!
+//
+//  Created by Brian Pinto on 1/7/23.
+//
+
+import Foundation
+
+
+//struct that contains strings properties used in creation of URL for Foursquare API Call
+struct Foursquare_API_Constants {
+    
+    static let API_Key = "fsq3JTL5+KcqJ1NavgKztGZqlH2zm9sYz1Ixk6NR4oKNkns="
+    static let API_URL = "https://api.foursquare.com/v3/places/search"
+}
+
+
+
+
+
+

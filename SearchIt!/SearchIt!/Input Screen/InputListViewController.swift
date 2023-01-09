a1df07f357dd76a9a82de2c4f97ca56966354a51
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-08 20:55:49 -0500

    Added list layout to collection view.

--- SearchIt!/SearchIt!/Input Screen/InputListViewController.swift
+++ SearchIt!/SearchIt!/Input Screen/InputListViewController.swift
@@ -7,4 +7,19 @@
 
 import UIKit
 
-
+class InputListViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfig = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfig.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfig)
+    }
+    
+}

e2858851d6ef2183edcac2ebdc16ab2bd1da43e7
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-10 16:42:41 -0500

    Added collection view controller to replace previous table view controller to prsent list of busiensses generated from API call.

---
+++ SearchIt!/SearchIt!/Business Screen/BusinessViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewController.swift
+//  SearchIt!
+//
+//  
+//
+
+import UIKit
+
+class BusinessViewController: UICollectionViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+
+    
+    
+}

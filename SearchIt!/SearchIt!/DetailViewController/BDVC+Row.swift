435908babdbdcd10f0ca069e0bb931e8043e30bf
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-09 21:22:59 -0500

    Added extension to BDVC with enumeration to customize details presented in detail view. Functions modify attached image and text style.

---
+++ SearchIt!/SearchIt!/DetailViewController/BDVC+Row.swift
@@ -0,0 +1,50 @@
+//
+//  BDVC+Row.swift
+//  SearchIt!
+//
+//  
+//
+
+import UIKit
+
+//Row enumeration to apply specific styling to each detail row of selected business
+
+extension BusinessDetailViewController {
+    
+    //Hashable protocol required by diffable data source that provide lists with data and styling
+    enum Row: Hashable {
+        case name
+        case address
+        case phone
+        case web
+        case description
+        case tips
+        
+        //SF Symbol next to detail row depending on case
+        var imageName: String? {
+            switch self {
+            case .address: return "globe.americas.fill"
+            case .phone: return "phone.circle"
+            case .web: return "wifi"
+            case .description: return "globe.americas.fill"
+            case .tips: return "scribble"
+            default: return nil
+            }
+        }
+        
+        //returns image based on image name
+        var image: UIImage? {
+            guard let imageName = imageName else { return nil }
+            let config = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: config)
+        }
+        
+        //returns text style based on case
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .name: return .headline
+            default: return .subheadline
+            }
+        }
+    }
+}

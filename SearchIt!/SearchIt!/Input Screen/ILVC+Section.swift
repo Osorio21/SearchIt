34710dbbe2454a8373703e994f2744e8e018ee83
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-10 03:43:41 -0500

    Cleaned up cases in enumeration.

--- SearchIt!/SearchIt!/Input Screen/ILVC+Section.swift
+++ SearchIt!/SearchIt!/Input Screen/ILVC+Section.swift
@@ -11,12 +11,12 @@ import Foundation
 extension InputListViewController {
     
     //enumeration to manage different sections
+    //data sources uses hash values to determine changes in data
     enum Section: Int, Hashable {
         case category //0
         case city //1
         case state //2
         case max_amount //3
-        case sort_by //4
         
         
         //name property for heading text
@@ -29,9 +29,7 @@ extension InputListViewController {
             case .state:
                 return NSLocalizedString("State", comment: "State location")
             case .max_amount:
-                return NSLocalizedString("Max Amount", comment: "Max number of results")
-            case .sort_by:
-                return NSLocalizedString("Sort By", comment: "Sorting preference")
+                return NSLocalizedString("Max Number of Results", comment: "Max number of results")
             }
         }
     }

ca8e181e5cccb576a698db79085ecda987464f28
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-10 16:30:58 -0500

    Renamed file and changed accessibility label.

---
+++ SearchIt!/SearchIt!/HomeScreen/HomeScreenController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  SearchIt!
+//
+//
+//
+
+import UIKit
+
+class HomeScreenController: UIViewController {
+    
+    //creation of application title variable
+    let appTitle = UILabel()
+    
+    //initialize gradient class
+    let colors = Colors()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //properties of app title text
+        appTitle.frame = CGRect(x: 0, y: 200, width: 250, height: 100)
+        appTitle.text = "SearchIt!"
+        appTitle.font = UIFont(name: "Gill Sans", size: 60)
+        appTitle.center.x = self.view.center.x
+        appTitle.textAlignment = .center
+        appTitle.layer.shadowColor = CGColor(red: 204.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1)
+        appTitle.layer.shadowRadius = 3
+        appTitle.layer.shadowOpacity = 1
+        appTitle.layer.shadowOffset = CGSize(width: 4, height: 4)
+        appTitle.layer.masksToBounds = false
+        
+        //add app title text to view
+        self.view.addSubview(appTitle)
+        
+        //create button for transition to second screen
+        let startItbutton = UIButton(frame: CGRect(x: 0, y: 600, width: 250, height: 100))
+        startItbutton.setTitle("StartIt!", for: .normal)
+        startItbutton.center.x = self.view.center.x
+        startItbutton.titleLabel?.font = UIFont(name: "Gill Sans", size: 40)
+        startItbutton.backgroundColor = UIColor.systemGray
+        startItbutton.accessibilityLabel = NSLocalizedString("Start App Button", comment: "Start button accessibility label")
+        startItbutton.addTarget(self, action: #selector(beginApp(sender:)), for: .touchUpInside)
+        self.view.addSubview(startItbutton)
+        
+        
+    }
+    
+    //set background
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        background_refresh()
+    }
+    
+    //adjust frame of background to transition with device orientation
+    override func viewDidLayoutSubviews() {
+        colors.cgl.frame = self.view.bounds
+    }
+    
+    //apply background colors to view
+    func background_refresh() {
+        view.backgroundColor = UIColor.clear
+        let backgroundLayer = colors.cgl
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, at: 0)
+      }
+    
+    //selector function for button
+    @objc func beginApp(sender: UIButton){
+        let inputVC = storyboard?.instantiateViewController(withIdentifier: "InputText") as! InputTextController
+        present(inputVC, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+}
+        
+    
+
+
+

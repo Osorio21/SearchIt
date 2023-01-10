884a333e65e8e37559e9657c5693c5e6b80fc040
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-10 03:45:24 -0500

    Cleaned up cell registrartion function and removed unnecessary button and background features.

--- SearchIt!/SearchIt!/Input Screen/ILVC+DataSource.swift
+++ SearchIt!/SearchIt!/Input Screen/ILVC+DataSource.swift
@@ -11,25 +11,29 @@ import UIKit
 
 extension InputListViewController {
     
-    typealias DS = UICollectionViewDiffableDataSource<Int,String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int,String>
+    typealias DS = UICollectionViewDiffableDataSource<Section,Row>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section,Row>
     
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
-        let r = ["This", "is", "a", "test."]
-        let a = r[indexPath.item]
-        var contentConfig = cell.defaultContentConfiguration()
-        contentConfig.text = a
-        cell.contentConfiguration = contentConfig
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, item: Row) {
+        
+        switch (item) {
+        case (.header(let title)):
+            cell.contentConfiguration = headerConfiguration(for: cell, with: title)
+        case (.text(_)):
+            cell.contentConfiguration = textFieldConfiguration(for: cell)
+        }
+        
+        cell.tintColor = UIColor(named: "TodayPrimaryTint")
         
         //configure background of list cells
-        var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
-        backgroundConfig.backgroundColor = UIColor(named: "TodayListCellBackground")
-        cell.backgroundConfiguration = backgroundConfig
+        //var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
+        //backgroundConfig.backgroundColor = UIColor(named: "TodayListCellBackground")
+        //cell.backgroundConfiguration = backgroundConfig
         
         //add pencil symbol to left side of field
-        var pencilSymbol = pencilSymbol()
-        pencilSymbol.tintColor = .black
-        cell.accessories = [ .customView(configuration: pencilSymbol), .disclosureIndicator(displayed: .always)]
+        //var pencilSymbol = pencilSymbol()
+        //pencilSymbol.tintColor = .black
+        //cell.accessories = [ .customView(configuration: pencilSymbol), .disclosureIndicator(displayed: .always)]
         
     }
     
@@ -37,7 +41,7 @@ extension InputListViewController {
     //change to switch based on section later
     private func pencilSymbol() -> UICellAccessory.CustomViewConfiguration {
         let pencil = "pencil.circle.fill"
-        let symbolConfig = UIImage.SymbolConfiguration(textStyle: .title1)
+        let symbolConfig = UIImage.SymbolConfiguration(textStyle: .body)
         let image = UIImage(systemName: pencil, withConfiguration: symbolConfig)
         let pencilButton = UIButton()
         pencilButton.setImage(image, for: .normal)

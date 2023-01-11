c31a87c72080e676c2a62d032be1168ef603fa69
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-10 21:13:07 -0500

    Edited properties.

--- SearchIt!/SearchIt!/Models/API_Constants.swift
+++ SearchIt!/SearchIt!/Models/API_Constants.swift
@@ -12,18 +12,21 @@ import Foundation
 struct Foursquare_API_Constants {
     
     static let API_Key = "fsq3JTL5+KcqJ1NavgKztGZqlH2zm9sYz1Ixk6NR4oKNkns="
+    static var Business_ID = "BusinessCell"
     var city: String
     var state: String
     var category: String
     var API_URL: String
     var max_amount: Int
+    var sort_by: String
     init(){
-        city = "San Francisco"
-        state = "CA"
+        city = "Seattle"
+        state = "WA"
         category = "pizza"
-        max_amount = 1
+        max_amount = 2
+        sort_by = "RELEVANCE"
         API_URL = """
-https://api.foursquare.com/v3/places/search?query=\(category)&fields=name%2Clocation%2Ctel%2Cwebsite%2Cfsq_id%2Cdescription%2Ctips&near=\(city)%2C%20\(state)&limit=\(max_amount)
+https://api.foursquare.com/v3/places/search?query=\(category)&fields=name%2Clocation%2Ctel%2Cwebsite%2Cfsq_id%2Cdescription&near=\(city)%2C\(state)&sort=\(sort_by)&limit=\(max_amount)
 """
     }
 }

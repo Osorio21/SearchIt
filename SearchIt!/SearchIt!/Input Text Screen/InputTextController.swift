6bf0f459d21530b00193dc3310104482fffff072
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-10 21:23:55 -0500

    Added APIInformation Protocol for info transfer to third screen.

--- SearchIt!/SearchIt!/Input Text Screen/InputTextController.swift
+++ SearchIt!/SearchIt!/Input Text Screen/InputTextController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 
+//protocol for sending input info to API Call Function
+protocol APIInformation {
+    func didEnterData(category_text: String, city_text: String, state_text: String, amount_text: String, sort_text: String) 
+}
+
 class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    var APIDelegate: APIInformation?
+    
+    //text labels for titles
     let cat_Title = UILabel()
     let loc_Title = UILabel()
     let num_Title = UILabel()
@@ -16,12 +24,15 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     
     let pickerArray = ["RELEVANCE", "DISTANCE", "POPULARITY", "RATING"]
     
-    var sort_TextField: UITextField!
-    
-    
     
     //initialize gradient class
     let backColors = Colors()
+    
+    //input text fields
+    let cat_TextField = UITextField(frame: CGRect(x: 10, y: 75, width: 350, height: 50))
+    let loc_TextField = UITextField(frame: CGRect(x: 10, y: 200, width: 350, height: 50))
+    let num_TextField = UITextField(frame: CGRect(x: 10, y: 330, width: 350, height: 50))
+    let sort_TextField = UITextField(frame: CGRect(x: 10, y: 455, width: 350, height: 50))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,13 +45,13 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         cat_Title.textAlignment = .center
         self.view.addSubview(cat_Title)
         
-        let cat_TextField = UITextField(frame: CGRect(x: 10, y: 75, width: 350, height: 50))
+        //properties for category text field
         cat_TextField.placeholder = "Enter product category..."
         cat_TextField.borderStyle = .roundedRect
         cat_TextField.accessibilityLabel = NSLocalizedString("Enter category", comment: "Category text field")
         view.addSubview(cat_TextField)
         
-        //properties for location button
+        //properties for location text
         loc_Title.frame = CGRect(x: 10, y: 125, width: 100, height: 100)
         loc_Title.text = "City,State:"
         loc_Title.font = UIFont(name: "Gill Sans", size: 20)
@@ -48,13 +59,13 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         loc_Title.textAlignment = .center
         self.view.addSubview(loc_Title)
         
-        let loc_TextField = UITextField(frame: CGRect(x: 10, y: 200, width: 350, height: 50))
+        //properties for location text field
         loc_TextField.placeholder = "Enter City,State..."
         loc_TextField.borderStyle = .roundedRect
         loc_TextField.accessibilityLabel = NSLocalizedString("Enter location", comment: "Location text field")
         view.addSubview(loc_TextField)
         
-        //properties for number of results button
+        //properties for number of results text
         num_Title.frame = CGRect(x: 0, y: 250, width: 180, height: 100)
         num_Title.text = "Max # of Results:"
         num_Title.font = UIFont(name: "Gill Sans", size: 20)
@@ -62,13 +73,13 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         num_Title.textAlignment = .center
         self.view.addSubview(num_Title)
         
-        let num_TextField = UITextField(frame: CGRect(x: 10, y: 330, width: 350, height: 50))
+        //properties for number of results text field
         num_TextField.placeholder = "Enter max number of results..."
         num_TextField.borderStyle = .roundedRect
         num_TextField.accessibilityLabel = NSLocalizedString("Enter number of results", comment: "Number of results text field")
         view.addSubview(num_TextField)
         
-        //properties for number of results button
+        //properties for sort by title
         sort_Title.frame = CGRect(x: 0, y: 375, width: 100, height: 100)
         sort_Title.text = "Sort by:"
         sort_Title.font = UIFont(name: "Gill Sans", size: 20)
@@ -76,13 +87,13 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         sort_Title.textAlignment = .center
         self.view.addSubview(sort_Title)
         
-        let sort_TextField = UITextField(frame: CGRect(x: 10, y: 455, width: 350, height: 50))
+        //properties for sort by text field
         sort_TextField.placeholder = "Sort by..."
         sort_TextField.borderStyle = .roundedRect
         sort_TextField.accessibilityLabel = NSLocalizedString("Enter sorting criteria", comment: "Sorting picker text field")
         view.addSubview(sort_TextField)
-        self.sort_TextField = sort_TextField
-        
+       
+        //picker initialization
         let picker = UIPickerView()
         picker.sizeToFit()
         picker.delegate = self
@@ -91,13 +102,13 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         sort_TextField.inputView = picker
         
         
-        //create button for transition to third screen
+        //create button for transition to business detail view screen
         let inputItbutton = UIButton(frame: CGRect(x: 0, y: 650, width: 250, height: 100))
         inputItbutton.setTitle("InputIt!", for: .normal)
         inputItbutton.center.x = self.view.center.x
         inputItbutton.titleLabel?.font = UIFont(name: "Gill Sans", size: 40)
         inputItbutton.backgroundColor = UIColor.systemGray
-        inputItbutton.addTarget(self, action: #selector(beginApp(sender:)), for: .touchUpInside)
+        inputItbutton.addTarget(self, action: #selector(inputData(sender:)), for: .touchUpInside)
         inputItbutton.accessibilityLabel = NSLocalizedString("Input Data Button", comment: "Input button accessibility label")
         self.view.addSubview(inputItbutton)
         
@@ -125,11 +136,25 @@ class InputTextController: UIViewController, UIPickerViewDelegate, UIPickerViewD
       }
     
     //selector function for button
-    @objc func beginApp(sender: UIButton){
+    //send data to Business Detail View Controller for API Call
+    @objc func inputData(sender: UIButton){
+        let cText = cat_TextField.text!
+        let lsText = loc_TextField.text!
+        let i = lsText.firstIndex(of: ",")
+        var range = lsText[..<i!]
+        let cityText = String(range)
+        let comma_break = lsText.index(i!, offsetBy: 1)
+        range = lsText[comma_break...]
+        let stateText = String(range)
+        let nText = num_TextField.text!
+        let sText = sort_TextField.text!
+        APIDelegate?.didEnterData(category_text: cText, city_text: cityText, state_text: stateText, amount_text: nText, sort_text: sText)
         let businessVC = storyboard?.instantiateViewController(withIdentifier: "BusinessView") as! BusinessViewController
         present(businessVC, animated: true, completion: nil)
         
     }
+    
+    //picker properties
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }

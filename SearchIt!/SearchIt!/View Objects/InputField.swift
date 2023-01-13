abe6cd04a154ec1a3db9384560ec3e1d420ae5be
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-13 00:57:42 -0500

    Edited comments and added borders.

--- SearchIt!/SearchIt!/View Objects/InputField.swift
+++ SearchIt!/SearchIt!/View Objects/InputField.swift
@@ -5,21 +5,27 @@
 //  
 //
 
+//custom UITextFIeld class
+
 import UIKit
 
 class InputField: UITextField {
     
     init(frame: CGRect, placeholder: String) {
         super.init(frame: frame)
+        
+        //set placeholder text
         self.placeholder = placeholder
+        
+        //borderstyle property
         self.borderStyle = .roundedRect
         
-        
+        //set black borders
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
-    
-    
 }

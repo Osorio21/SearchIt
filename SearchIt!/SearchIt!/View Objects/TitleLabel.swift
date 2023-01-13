f368ad97bccf0a7108b0ff50a950edfda34217de
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-13 00:59:25 -0500

    Added comments and shadow properties.

--- SearchIt!/SearchIt!/View Objects/TitleLabel.swift
+++ SearchIt!/SearchIt!/View Objects/TitleLabel.swift
@@ -5,16 +5,26 @@
 //  
 //
 
+//custom UILabel for application title
+
 import UIKit
 
 class TitleLabel: UILabel {
     
     init(frame: CGRect, title: String) {
         super.init(frame: frame)
+        
+        //set text properties
         self.text = title
         self.font = UIFont(name: "Gill Sans", size: 60)
         self.textAlignment = .center
         
+        //set shadow properties
+        self.layer.shadowColor = CGColor(red: 204.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1)
+        self.layer.shadowRadius = 3
+        self.layer.shadowOpacity = 1
+        self.layer.shadowOffset = CGSize(width: 4, height: 4)
+        self.layer.masksToBounds = false
     }
     
     required init?(coder: NSCoder) {

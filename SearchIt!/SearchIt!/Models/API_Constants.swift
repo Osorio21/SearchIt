074788c3c2b856d3494b7410c0510c74ce9386c3
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-14 04:01:26 -0500

    Created custom intializer for future creation of objects.

--- SearchIt!/SearchIt!/Models/API_Constants.swift
+++ SearchIt!/SearchIt!/Models/API_Constants.swift
@@ -11,22 +11,26 @@ import Foundation
 //struct that contains strings properties used in creation of URL for Foursquare API Call
 struct Foursquare_API_Constants {
     
+    //declare values in URL
     static let API_Key = "fsq3JTL5+KcqJ1NavgKztGZqlH2zm9sYz1Ixk6NR4oKNkns="
-    static var Business_ID = "BusinessCell"
     var city: String
     var state: String
     var category: String
     var API_URL: String
-    var max_amount: Int
+    var max_amount: String
     var sort_by: String
-    init(){
-        city = "Seattle"
-        state = "WA"
-        category = "pizza"
-        max_amount = 2
-        sort_by = "RELEVANCE"
+    
+    //default values
+    init(_ city: String = "Seattle", _ state: String = "WA", _ category: String = "pizza", _ max_amount: String = "2", _ sort_by: String = "RELEVANCE"){
+        
+        self.city = city
+        self.state = state
+        self.category = category
+        self.max_amount = max_amount
+        self.sort_by = sort_by
+        
         API_URL = """
-https://api.foursquare.com/v3/places/search?query=\(category)&fields=name%2Clocation%2Ctel%2Cwebsite%2Cfsq_id%2Cdescription&near=\(city)%2C\(state)&sort=\(sort_by)&limit=\(max_amount)
+https://api.foursquare.com/v3/places/search?query=\(self.category)&fields=name%2Clocation%2Ctel%2Cwebsite%2Cfsq_id%2Cdescription&near=\(self.city)%2C\(self.state)&sort=\(self.sort_by)&limit=\(self.max_amount)
 """
     }
 }

2c467e26c61061595ff7e7e62b7b23ba7fd8404c
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-15 20:39:30 -0500

    Created viewcontroller to display information about business selected in cell of BusinessTableViewController.

---
+++ SearchIt!/SearchIt!/DetailViewController/BusinessDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BusinessDetailsViewController.swift
+//  SearchIt!
+//
+//  Created by Brian Pinto on 1/15/23.
+//
+
+import UIKit
+
+class BusinessDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 178.0/255.0, green: 102.0/255.0, blue: 1, alpha: 1.0)
+    }
+}

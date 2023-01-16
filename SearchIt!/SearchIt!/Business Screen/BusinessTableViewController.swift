e5373e9c7b840ff5a3999a58d1425f9b5086213a
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-15 21:12:07 -0500

    Edited comments.

--- SearchIt!/SearchIt!/Business Screen/BusinessTableViewController.swift
+++ SearchIt!/SearchIt!/Business Screen/BusinessTableViewController.swift
@@ -9,26 +9,38 @@ import UIKit
 
 class BusinessTableViewController: UITableViewController {
     
+    //declare business array to hold Business objects after API call/JSON decoding
     var businesses = [Business]()
-    let constant = Foursquare_API_Constants()
+    
+    //declare Foursquare_API_Constants object transferred from InputTextController
     var input: Foursquare_API_Constants?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //set background color
         tableView.backgroundColor = UIColor(red: 0.0, green: 102.0/255.0, blue: 204.0/255.0, alpha: 1.0)
         
+        //create custom view
         let customView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 150))
+        
+        //create button to transition back one screen
         let rewindItbutton = TransitionButton(frame: CGRect(x: 0, y: 0, width: 250, height: 100), label: "RewindIt!")
+        
+        //button properties and action
         rewindItbutton.accessibilityLabel = NSLocalizedString("Rewind Button", comment: "Rewind button accessibility label")
         rewindItbutton.addTarget(self, action: #selector(returnScreen(sender:)), for: .touchUpInside)
         rewindItbutton.center.x = view.center.x
         rewindItbutton.center.y = customView.center.y
         
+        //add button to custom view and add custom view to footer view of table
         customView.addSubview(rewindItbutton)
         tableView.tableFooterView = customView
         
+        //force unrap F_A_C object created in ITC
         let inputData = input!
+        
+        //API Call and JSON decoding process
         
         //headers
         let headers = [
@@ -70,14 +82,17 @@ class BusinessTableViewController: UITableViewController {
         dataTask.resume()
     }
     
+    //sets number of table cells to number of business objects generated from API call
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.businesses.count
     }
     
+    //sets table cell height
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100.0
     }
     
+    //customizes data, text, and color of table cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let business = businesses[indexPath.row]
@@ -97,10 +112,14 @@ class BusinessTableViewController: UITableViewController {
         return cell
     }
     
+    //loads BusinessDetailsViewController and transfers Business object data associated with touched cell
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let businessDVC = storyboard?.instantiateViewController(withIdentifier: "BusinessDetails") as! BusinessDetailsViewController
+        businessDVC.business = businesses[indexPath.row]
         present(businessDVC, animated: true, completion: nil)
     }
+    
+    //dismisses view after RewindIt! button is pressed
     @objc func returnScreen(sender: UIButton) {
         dismiss(animated: true)
     }

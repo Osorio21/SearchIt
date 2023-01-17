24a4a72c8920f4f517eb0250f4b84d68f46229ad
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-16 22:56:01 -0500

    Added map button to footer view and adjusted its properties.

--- SearchIt!/SearchIt!/DetailViewScreen/BusinessDetailsViewController.swift
+++ SearchIt!/SearchIt!/DetailViewScreen/BusinessDetailsViewController.swift
@@ -21,21 +21,30 @@ class BusinessDetailsViewController: UITableViewController {
         //set background color
         tableView.backgroundColor = UIColor(red: 153.0/255.0, green: 153.0/255.0, blue: 1, alpha: 1.0)
         
-        //create custom view
-        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 150))
+        //create custom views
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 150))
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 75))
         
-        //create button to transition back one screen
-        let rewindItbutton = TransitionButton(frame: CGRect(x: 0, y: 0, width: 250, height: 100), label: "RewindIt!")
+        //create back button and map button
+        let rewindItbutton = TransitionButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50), label: "RewindIt!")
+        let mapItbutton = TransitionButton(frame: CGRect(x: 0, y: 0, width: 250, height: 100), label: "MapIt!")
         
-        //button properties and action
+        //back button properties and action
         rewindItbutton.accessibilityLabel = NSLocalizedString("Rewind Button", comment: "Rewind button accessibility label")
         rewindItbutton.addTarget(self, action: #selector(returnScreen(sender:)), for: .touchUpInside)
         rewindItbutton.center.x = view.center.x
-        rewindItbutton.center.y = customView.center.y
+        rewindItbutton.center.y = headerView.center.y
         
-        //add button to custom view and add custom view to footer view of table
-        customView.addSubview(rewindItbutton)
-        tableView.tableFooterView = customView
+        //map button properties
+        mapItbutton.accessibilityLabel = NSLocalizedString("Map Button", comment: "Map button accessibility label")
+        mapItbutton.center.x = view.center.x
+        mapItbutton.center.y = footerView.center.y
+        
+        //add buttons to views and add views to header/footer views of table
+        headerView.addSubview(rewindItbutton)
+        footerView.addSubview(mapItbutton)
+        tableView.tableFooterView = footerView
+        tableView.tableHeaderView = headerView
     }
     
     //set number of sections
@@ -54,6 +63,7 @@ class BusinessDetailsViewController: UITableViewController {
         label.textColor = UIColor(red: 64.0/255.0, green: 64.0/255.0, blue: 64.0/255.0, alpha: 1)
         label.text = self.headerTitles[section]
         label.font = UIFont.boldSystemFont(ofSize: 25)
+        label.textAlignment = .center
         return label
     }
     

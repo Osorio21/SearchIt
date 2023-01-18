e917ae07a1fac6c469b4f68d8fce30dec321b65c
Brian Pinto <brianpinto@Brians-MacBook-Pro.local>
2023-01-17 20:16:39 -0500

    Edited initializer to clean code in InputTextController.

--- SearchIt!/SearchIt!/Models/BackgroundScreenColor.swift
+++ SearchIt!/SearchIt!/Models/BackgroundScreenColor.swift
@@ -5,17 +5,21 @@
 //  
 //
 
+//custom struct that provides background color gradient for views
+
 import UIKit
 
 struct BackgroundColors {
-  var colorTop = CGColor(red: 0.0/255.0, green: 128.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-  var colorBottom = CGColor(red: 102.0/255.0, green: 178.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    
+    var colorTop: CGColor
+    var colorBottom: CGColor
+    let cgl: CAGradientLayer
 
-  let cgl: CAGradientLayer
-
-  init() {
-    cgl = CAGradientLayer()
-    cgl.colors = [ colorTop, colorBottom]
-    cgl.locations = [ 0.0, 1.0]
+    init(cgl: CAGradientLayer = CAGradientLayer(), colorTop: CGColor = CGColor(red: 0.0/255.0, green: 128.0/255.0, blue: 255.0/255.0, alpha: 1.0), colorBottom: CGColor = CGColor(red: 102.0/255.0, green: 178.0/255.0, blue: 255.0/255.0, alpha: 1.0)) {
+        self.cgl = cgl
+        self.colorTop = colorTop
+        self.colorBottom = colorBottom
+        self.cgl.colors = [self.colorTop, self.colorBottom]
+        self.cgl.locations = [ 0.0, 1.0]
   }
 }
